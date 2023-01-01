8ea2bfdb447279e78b8e088b149f6b07aed74a5d
2020mini.fsuke1985 <fsuke1985@gmail.com>
2023-01-01 07:56:38 +0000

    a

--- main.go
+++ main.go
@@ -1,18 +1,47 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"log"
 	"net/http"
 )
 
+type JsonStruct struct {
+	x string
+	y int
+	j []string
+}
+
 func main() {
+
+	type JsonStruct struct {
+		x string
+		y int
+		j []string
+	}
 
 	helloHandler := func(w http.ResponseWriter, req *http.Request) {
 		io.WriteString(w, "Hello, World\n")
 	}
 
-	http.HandleFunc("/", helloHandler)
+	jsonHandler := func(w http.ResponseWriter, req *http.Request) {
+		jsons := JsonStruct{
+			x: "named",
+			y: 1,
+			j: []string{"a", "b", "c"},
+		}
+		i, err := json.Marshal(jsons)
+
+		_ = i
+
+		if err != nil {
+
+		}
+
+	}
+
+	http.HandleFunc("/", jsonHandler)
 	http.HandleFunc("/hello", helloHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
