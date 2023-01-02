fd8912a07acbb9fee26b31bfb4069c594398ab9a
2020mini.fsuke1985 <fsuke1985@gmail.com>
2023-01-02 05:30:47 +0000

    ルートページの作成 Fixes #9

---
+++ src/client/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Index() {
+    return (
+        <>
+            <Head>
+                <title>pageIndex</title>
+            </Head>
+            <h1>pageIndex</h1>
+        </>
+    )
+}
