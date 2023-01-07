7348a8f76d7bcc556d9437bb27f292cc3170a4a8
fsuke1985 <fsuke1985@gmail.com>
2023-01-07 07:43:31 +0000

    設定と初期ページの作成できた

--- src/client/pages/index.tsx
+++ src/client/pages/index.tsx
@@ -1,24 +1,54 @@
 import Head from 'next/head'
 import useAppleMusicSWR from '../lib/hooks/useAppleMusicSWR'
+import Button from '@nextui-org/react/button';
+import { Container, Grid, Card, Col, Row, Text } from "@nextui-org/react";
+import { isPropertySignature } from 'typescript';
+
+const BtnComponent = () => <Button>Click me</Button>
 
 export default function Index() {
-    const {mutate, data, error, isValidating, isLoading} = useAppleMusicSWR()
-console.log(data)
+//    const {mutate, data, error, isValidating, isLoading} = useAppleMusicSWR()
+
+    type CardConteinerProps = {
+        text: string;
+    }
+
+    const CardContainer = (props: CardConteinerProps) => {
+
+        return (
+            <Card css={{ $$cardColor: '$colors$neutral' }}>
+                <Card.Body>
+                    <Row justify="center" align="center">
+                        <Text h6 size={15} color="white" css={{ m: 0 }}>
+                            {props.text}
+                        </Text>
+                    </Row>
+                </Card.Body>
+            </Card>
+        )
+    }
     return (
         <>
             <Head>
                 <title>pageIndex</title>
             </Head>
-            <h1>pageIndex</h1>
-            <div>
-                <p>
-                {
-                    isLoading? 'loading...' : data.X
-                }
-                </p>
-
-            </div>
-            
+            <Grid.Container gap={2} justify="center">
+                <Grid xs={4}>
+                    <CardContainer text="1 of 3" />
+                </Grid>
+                <Grid xs={4}>
+                    <CardContainer text="1 of 3" />
+                </Grid>
+                <Grid xs={4}>
+                    <CardContainer text="1 of 3" />
+                </Grid>
+                <Grid xs={4}>
+                    <CardContainer text="1 of 3" />
+                </Grid>
+                <Grid xs={4}>
+                    <CardContainer text="1 of 3" />
+                </Grid>
+            </Grid.Container>
         </>
     )
 }
